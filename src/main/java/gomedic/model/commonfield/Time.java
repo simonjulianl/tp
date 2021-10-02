248c65778f0743f749a093845777a312075d2cf1
Simon Julian Lauw <simonjulianl@u.nus.edu>
2021-10-02 16:55:00 +0800

    Fix checkstyle

--- src/main/java/gomedic/model/commonfield/Time.java
+++ src/main/java/gomedic/model/commonfield/Time.java
@@ -4,7 +4,6 @@ import static java.util.Objects.requireNonNull;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.time.format.FormatStyle;
 
 import gomedic.commons.util.CollectionUtil;
 
