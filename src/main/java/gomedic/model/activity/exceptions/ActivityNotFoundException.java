c93625aa21876ae2d93379c22b5c6c697b51c784
Simon Julian Lauw <simonjulianl@u.nus.edu>
2021-10-03 15:08:54 +0800

    Finish creating activity model

---
+++ src/main/java/gomedic/model/activity/exceptions/ActivityNotFoundException.java
@@ -0,0 +1,7 @@
+package gomedic.model.activity.exceptions;
+
+/**
+ * Signals that the operation is unable to find the specified activity.
+ */
+public class ActivityNotFoundException extends RuntimeException {
+}
