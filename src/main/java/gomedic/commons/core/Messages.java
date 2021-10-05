96952f806d2fa9c3187613481966ff1f27a23b9a
ngsengleng <ngsengleng@gmail.com>
2021-10-05 22:09:36 +0800

    Change log message for invalid command to reflect closest commands

--- src/main/java/gomedic/commons/core/Messages.java
+++ src/main/java/gomedic/commons/core/Messages.java
@@ -31,11 +31,15 @@ public class Messages {
                 "clear", "find", "list", "exit", "help");
         LevenshteinDistance stringChecker = new LevenshteinDistance();
         List<Pair<Integer, String>> closestStrings = listOfCommands.stream()
-                .map(x -> new Pair<>(stringChecker.apply(x, command), x)).sorted(Comparator.comparingInt(Pair::getKey))
+                .map(x -> new Pair<>(stringChecker.apply(x, command), x))
+                .sorted(Comparator.comparingInt(Pair::getKey))
                 .collect(Collectors.toList());
 
         String reply = String.format("Sorry, %s is an invalid command. "
                 + "You can choose from these commands instead: \n", command);
+        for (Pair<Integer, String> s : closestStrings) {
+            System.out.println(s);
+        }
         Iterator<Pair<Integer, String>> iterator = closestStrings.subList(0, 5).iterator();
         while (iterator.hasNext()) {
             reply += iterator.next().getValue() + "\n";
