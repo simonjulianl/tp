9cf5cceab0450791da046a21f342e78d46f62c20
Simon Julian Lauw <simonjulianl@u.nus.edu>
2021-10-21 17:43:38 +0800

    Optimize imports

--- src/test/java/gomedic/logic/commands/ReferralCommandTest.java
+++ src/test/java/gomedic/logic/commands/ReferralCommandTest.java
@@ -1,7 +1,6 @@
 package gomedic.logic.commands;
 
 import static gomedic.testutil.TypicalPersons.getTypicalAddressBook;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
