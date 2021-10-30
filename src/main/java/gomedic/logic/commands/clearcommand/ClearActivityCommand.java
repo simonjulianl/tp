8049c33ba0d6797976dd21f2f3d6c553da39f7e6
yyhangz <y.y.hangz@hotmail.com>
2021-10-30 12:39:57 +0800

    Fix bug where clear/type clears the user profile as well

--- src/main/java/gomedic/logic/commands/clearcommand/ClearActivityCommand.java
+++ src/main/java/gomedic/logic/commands/clearcommand/ClearActivityCommand.java
@@ -23,6 +23,7 @@ public class ClearActivityCommand extends Command {
         requireNonNull(model);
         AddressBook newAddressBook = new AddressBook();
         ReadOnlyAddressBook oldAddressBook = model.getAddressBook();
+        newAddressBook.setUserProfile(oldAddressBook.getUserProfile());
         newAddressBook.setDoctors(oldAddressBook.getDoctorListSortedById());
         newAddressBook.setPatients(oldAddressBook.getPatientListSortedById());
         model.setAddressBook(newAddressBook);
